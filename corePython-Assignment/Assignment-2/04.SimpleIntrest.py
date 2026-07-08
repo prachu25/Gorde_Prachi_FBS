ccc671199b8cbac8329a11f245eef9bb2d7bdb2d
prachu25 <gordeprachi64@gmail.com>
2026-07-08 08:19:21 +0530

    Initial commit

---
+++ corePython-Assignment/Assignment-2/04.SimpleIntrest.py
@@ -0,0 +1,11 @@
+# Write a program to enter P, T, R and calculate simple Interest.
+
+p = float(input('Enter a principal: '))
+r = float(input('Enter a rate: '))
+t = float(input('Enter time: '))
+
+simple_intrest = (p * t * r)/100
+
+print('Simple Intrest: ',simple_intrest)
+
+
