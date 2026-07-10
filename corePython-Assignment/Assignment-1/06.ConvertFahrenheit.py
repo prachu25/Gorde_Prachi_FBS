2446b4d00511a72b1dffc90cedb9edd8ece18e04
Prachi Gorde <gordeprachi64@gmail.com>
2026-07-10 14:36:06 +0530

    Delete corePython-Assignment directory

--- corePython-Assignment/Assignment-1/06.ConvertFahrenheit.py
+++
@@ -1,7 +0,0 @@
-# write a program to convert celsius to fahrenheit
-
-celsius = float(input('Enter a Temperature in Celsius: '))
-
-fahrenheit = (9 * celsius / 5) + 32
-
-print('Temperature in fahrenheit',fahrenheit)
