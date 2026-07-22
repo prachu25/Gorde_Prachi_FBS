e5006d68b9fb1ffa44d7f646e7dfbb4383c3bb7f
prachu25 <gordeprachi64@gmail.com>
2026-07-22 18:25:11 +0530

    Added Python pattern folder

---
+++ CorePython/Patterns/pattern2.py
@@ -0,0 +1,13 @@
+for i in range(1,6):
+    for j in range(1,6):
+        print(i, end=" ")     # same value in rows use i
+    print()
+
+
+print()
+print("NEW Pattern For Column")
+for l in range(1,6):
+    for m in range(1,6):
+        print(m, end=" ")     # same value in columns use j
+    print()
+
